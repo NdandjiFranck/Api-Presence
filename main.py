7693f67e05e3aaaa27c8ccde1a1a48c3f41e5353
NdandjiFranck <94813639+NdandjiFranck@users.noreply.github.com>
2023-11-07 23:37:30 +0100

    commit

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from documentations.tags import tags_metadata
 
 #Routers
 import routers.router_players
-import routers.router_teams
+#import routers.router_teams
 import routers.router_auth
 # Initialisation de l'API
 app = FastAPI(
-    title="Attendance Tracker",
+    title="Players Affiliation",
     description=api_description,
     openapi_tags= tags_metadata
 )
 
 # Router dédié aux players
 app.include_router(routers.router_players.router)
-app.include_router(routers.router_teams.router)
+#app.include_router(routers.router_teams.router)
 app.include_router(routers.router_auth.router)
 
