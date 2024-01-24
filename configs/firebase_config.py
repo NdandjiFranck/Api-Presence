bee50a355aff36dd1c2682ce6a20a1d2d6856c55
NdandjiFranck <94813639+NdandjiFranck@users.noreply.github.com>
2024-01-24 11:09:15 +0100

    commit

--- configs/firebase_config.py
+++ configs/firebase_config.py
@@ -1,11 +1,10 @@
 firebaseConfig = {
-  "apiKey": "AIzaSyBAm-WqK_1DZXiNQTR_KmfIL717npNDx-o",
-  "authDomain": "api-presence.firebaseapp.com",
-  "databaseURL": "https://api-presence-default-rtdb.europe-west1.firebasedatabase.app",
-  "projectId": "api-presence",
-  "storageBucket": "api-presence.appspot.com",
-  "messagingSenderId": "705905376999",
-  "appId": "1:705905376999:web:7dc898df8dda914925ac42",
-  "measurementId": "G-45JCB8L0KZ",
-  "databaseURL": "https://api-presence-default-rtdb.europe-west1.firebasedatabase.app/"
+    "apiKey": "AIzaSyCr7-SkWdfyONLzbmPt_6nPY2prsQdKhJU",
+    "authDomain": "api-python-f035b.firebaseapp.com",
+    "projectId": "api-python-f035b",
+    "storageBucket": "api-python-f035b.appspot.com",
+    "messagingSenderId": "307225626620",
+    "appId": "1:307225626620:web:37db66adaa7bc9766a9479",
+    "measurementId": "G-3RZLG690Z7",
+  "databaseURL": "https://api-python-f035b-default-rtdb.europe-west1.firebasedatabase.app/"
 }
